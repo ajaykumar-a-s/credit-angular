1ef70d75623266c101849e5d7f4c06345445a698
Aswinraj S <aswinraj.ksraj@gmail.com>
2024-03-19 18:01:02 +0530

    aswin frontend

--- src/app/components/card/view-cards/view-cards.component.ts
+++ src/app/components/card/view-cards/view-cards.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
-
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CardComponent } from '../card.component';
+import { FormsModule } from '@angular/forms';
+import { NgFor, NgIf } from '@angular/common';
+import { CardService } from '../../../services/card.service';
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-view-cards',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, CardComponent,RouterLink,RouterLinkActive,FormsModule,NgFor,NgIf],
   templateUrl: './view-cards.component.html',
   styleUrl: './view-cards.component.css'
 })
 export class ViewCardsComponent {
+  cardList: any[] = [];
+  constructor(private cardService: CardService,private http: HttpClient) {
+    this.cardService.cardList().subscribe(
+      { next: (response) => {
+        this.cardList = response;
+      },
+      error: (error) => {
+        console.log(error);
+      }
+    });
+  }
 
 }
