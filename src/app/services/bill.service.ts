011e4bd868b09816f6d4ef1173bb867b23586b1e
Surandhar <160579474+Surandharg@users.noreply.github.com>
2024-03-19 19:30:21 +0530

    done without style

--- src/app/services/bill.service.ts
+++ src/app/services/bill.service.ts
@@ -1,9 +1,42 @@
+import { HttpClient,HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import{BillResponse} from '../model/bill';
+import { Observable,catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BillService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+getBill(
+  creditCardNumber : string
+):Observable<BillResponse>{
+  return this.http
+  .get<BillResponse>(
+    'http://localhost:8080/getBill/' + creditCardNumber
+  )
+  .pipe(catchError(this.handleError));
+}
+payBill(
+  creditCardNumber : string
+):Observable<any>{
+  return this.http
+  .get<any>(
+    'http://localhost:8080/billPayment/' + creditCardNumber,
+    
+  )
+  .pipe(catchError(this.handleError));
+}
+
+handleError(httpError: HttpErrorResponse): Observable<never> {
+  let errorMessage: string = '';
+  if (httpError.error instanceof ErrorEvent) {
+    errorMessage = `Error: ${httpError.error.message}`;
+  } else {
+    errorMessage = `Error Code: ${httpError.status}\nMessage: ${httpError.error}`;
+  }
+  return throwError(errorMessage);
+}
+
 }
