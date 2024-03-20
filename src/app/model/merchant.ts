af6141fe43208f24f049ec1f84ec3f41fafbe73e
Srinidhi R <ssrinidhisrilu@gmail.com>
2024-03-20 16:30:09 +0530

    Update merchant.ts

--- src/app/model/merchant.ts
+++ src/app/model/merchant.ts
@@ -1,12 +1,6 @@
-export class Merchant {
-    
-    merchantId?: number;
-    name?: string;
-    email?: string;
-    password?: string;
-    phone?: string;
-    address?: string;
-    dateOfBirth?: Date;
-    balance?: number;
-    cardNumber? : string;
+export interface Merchant {
+    merchantId ?: number;
+    name : string;
+    balance : number;
+    cardNumber : string;
 }
