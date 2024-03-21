4cda008042ebb38a738b1235c906dc513287390e
SAMYUKTHA KRP <krpsamyuktha2002@gmail.com>
2024-03-21 20:21:18 +0530

    customer implementation angular

--- src/app/components/customer/customer.component.ts
+++ src/app/components/customer/customer.component.ts
@@ -1,14 +1,55 @@
-import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { CustomerService } from '../../services/customer.service';
+import { LoginService } from '../../services/login.service';
 
 @Component({
   selector: 'app-customer',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, HeaderComponent, CommonModule],
   templateUrl: './customer.component.html',
   styleUrl: './customer.component.css'
 })
-export class CustomerComponent {
-
+export class CustomerComponent implements OnInit{
+  customers !: any;
+  errorMessage !: string;
+  constructor(private customerService: CustomerService, private loginService : LoginService, private router : Router) {}
+  ngOnInit(): void {
+    if (this.loginService.isAdminLoggedIn()) {
+      this.customerService.getAllCustomers().subscribe({
+        next: (customers) => {
+          this.customers = customers;
+        },
+        error: (error) => {
+          this.errorMessage = error;
+        }
+      })
+    }else{
+      this.customerService.getCustomerById(this.loginService.getCustomer().customerId).subscribe({
+        next: (customer) => {
+          this.customers = [customer];
+        },
+        error: (error) => {
+          this.errorMessage = error;
+        }
+      })
+    }
+  }
+  deleteCustomer(customerId: number){
+    this.customerService.deleteCustomer(customerId).subscribe({
+      next: () => {
+        this.customers = this.customers.filter((customer: { customerId: number; }) => customer.customerId !== customerId);
+      },
+      error: (error) => {
+        this.errorMessage = error;
+      }
+    })
+  }
+  updateCustomer(id : number){
+    this.customerService.setCurrentCustomer(this.customers[id]);
+    this.router.navigate(['/update-customer'])
+  }
 }
--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -1,9 +1,41 @@
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
+  currentCustomer : any;
 
-  constructor() { }
+  constructor(private http : HttpClient) { }
+  addCustomer(customer: any){
+    return this.http.post('http://localhost:8080/customer', customer).pipe(catchError(this.handleError));
+  }
+  getCustomerById(customerId: number): Observable<any> {
+    return this.http.get<any>('http://localhost:8080/customer/' + customerId).pipe(catchError(this.handleError));
+  
+  }
+  getAllCustomers(): Observable<any[]> {
+    return this.http.get<any[]>('http://localhost:8080/customers').pipe(catchError(this.handleError));
+  }
+  deleteCustomer(customerId: number): Observable<any> {
+    return this.http.delete<any>('http://localhost:8080/customer/' + customerId).pipe(catchError(this.handleError));
+  
+  }
+  handleError(httpError: HttpErrorResponse): Observable<never> {
+    let errorMessage: string = '';
+    if (httpError.error instanceof ErrorEvent) {
+      errorMessage = `Error: ${httpError.error.message}`;
+    } else {
+      errorMessage = `Error Code: ${httpError.status}\nMessage: ${httpError.error}`;
+    }
+    return throwError(errorMessage);
+  }
+  public setCurrentCustomer(customer: any){
+    this.currentCustomer = customer;
+  }
+  public getCurrentCustomer(){
+    return this.currentCustomer;
+  }
 }
