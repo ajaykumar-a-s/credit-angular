cd20e01b8a9275f798044292d1f2058d8441b4d8
Ajaykumar A S <asajay1902@gmail.com>
2024-03-22 14:20:42 +0530

    ui changes

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -3,15 +3,17 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "../header/header.component";
 import { FormsModule } from '@angular/forms';
 import { LoginService } from '../../services/login.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-card',
     standalone: true,
     templateUrl: './card.component.html',
     styleUrl: './card.component.css',
-    imports: [RouterOutlet, HeaderComponent,RouterLink,RouterLinkActive,FormsModule]
+    imports: [RouterOutlet, HeaderComponent,RouterLink,RouterLinkActive,FormsModule, CommonModule]
 })
 export class CardComponent {
     constructor(private loginService: LoginService) {}
     isCustomer = this.loginService.isCustomerLoggedIn();
+    isAdmin = this.loginService.isAdminLoggedIn();
 }
